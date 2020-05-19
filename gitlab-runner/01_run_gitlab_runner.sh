a7835126ab1d0ffab91e5e287ac5e7218a9169f0
Jakub Dubovsky <jakub.dubovsky@seznam.cz>
2020-05-19 07:58:56 +0200

    expose kong on api.domain

--- gitlab-runner/01_run_gitlab_runner.sh
+++ gitlab-runner/01_run_gitlab_runner.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker run -d \
-    --name gitlab-runner \
     --restart always \
     -v /var/opt/gitlab-runner:/etc/gitlab-runner \
     -v /var/run/docker.sock:/var/run/docker.sock \
